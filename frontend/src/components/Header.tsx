070cef9cbf0b6c987dae9019bfcd8a5461081ced
Cristian <cristiansf695@hotmail.com>
2022-09-05 11:33:31 -0300

    Other components

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import logo from '../assets/img/image 1.svg'
+import './Header.css'
+
+export function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://www.instagram.com/devsuperior.ig">@Devsuperior.ig</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
